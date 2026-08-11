0277c3482b5baaf571382b46356afa68c3bcc01d
Le Vivilet <levivilet@pm.me>
2026-08-11 06:33:00 -0700

    fix: improve Erlang syntax highlighting (#38)

--- src/tokenizeErlang.js
+++ src/tokenizeErlang.js
@@ -3,63 +3,74 @@
  */
 export const State = {
   TopLevelContent: 1,
+  InsideDoubleQuoteString: 2,
+  InsideQuotedAtom: 3,
 }
 
 /**
  * @enum number
  */
 export const TokenType = {
+  None: 1,
   Whitespace: 2,
-  Punctuation: 3,
-  CurlyOpen: 6,
-  CurlyClose: 7,
-  Variable: 10,
-  None: 57,
-  Unknown: 881,
-  Numeric: 883,
-  NewLine: 884,
-  Comment: 885,
-  Query: 886,
-  Text: 887,
-  CssSelectorId: 889,
-  FuntionName: 890,
-  String: 891,
-  KeywordImport: 892,
+  PunctuationString: 3,
+  String: 4,
+  Keyword: 5,
+  Numeric: 6,
+  Punctuation: 7,
+  VariableName: 8,
+  Comment: 9,
+  Text: 10,
+  LanguageConstant: 11,
+  Function: 12,
+  KeywordControl: 13,
+  Macro: 14,
 }
 
 export const TokenMap = {
-  [TokenType.Whitespace]: 'Whitespace',
-  [TokenType.Punctuation]: 'Punctuation',
-  [TokenType.CurlyOpen]: 'Punctuation',
-  [TokenType.CurlyClose]: 'Punctuation',
-  [TokenType.Variable]: 'VariableName',
   [TokenType.None]: 'None',
-  [TokenType.Unknown]: 'Unknown',
-  [TokenType.Numeric]: 'Numeric',
-  [TokenType.NewLine]: 'NewLine',
-  [TokenType.Comment]: 'Comment',
-  [TokenType.Query]: 'CssAtRule',
-  [TokenType.Text]: 'Text',
-  [TokenType.CssSelectorId]: 'CssSelectorId',
-  [TokenType.FuntionName]: 'Function',
+  [TokenType.Whitespace]: 'Whitespace',
+  [TokenType.PunctuationString]: 'PunctuationString',
   [TokenType.String]: 'String',
-  [TokenType.KeywordImport]: 'KeywordImport',
+  [TokenType.Keyword]: 'Keyword',
+  [TokenType.Numeric]: 'Numeric',
+  [TokenType.Punctuation]: 'Punctuation',
+  [TokenType.VariableName]: 'VariableName',
+  [TokenType.Comment]: 'Comment',
+  [TokenType.Text]: 'Text',
+  [TokenType.LanguageConstant]: 'LanguageConstant',
+  [TokenType.Function]: 'Function',
+  [TokenType.KeywordControl]: 'KeywordControl',
+  [TokenType.Macro]: 'Macro',
 }
 
-const RE_ANYTHING = /^.+/s
-const RE_LINE_COMMENT = /^%.*/
-const RE_NUMERIC =
-  /^((0(x|X)[0-9a-fA-F]*)|(([0-9]+\.?[0-9]*)|(\.[0-9]+))((e|E)(\+|-)?[0-9]+)?)\b/
-
 const RE_WHITESPACE = /^\s+/
-const RE_VARIABLE_NAME = /^[a-zA-Z_$][a-zA-Z\d\_]*/
-const RE_PUNCTUATION = /^[\(\)=\+\-><\.:;\{\}\[\]!,&\|\^\?\*%~]/
+const RE_COMMENT = /^%.*/
+const RE_CONTROL_KEYWORD =
+  /^(?:after|begin|case|catch|cond|else|end|fun|if|let|maybe|of|receive|try|when)\b/
+const RE_KEYWORD =
+  /^(?:and|andalso|band|bnot|bor|bsl|bsr|bxor|div|not|or|orelse|rem|xor)\b/
+const RE_MACRO = /^\?(?:[a-zA-Z_][a-zA-Z\d_@]*|'(?:\\.|[^'\\])*')/
+const RE_NUMBER =
+  /^(?:\d+#[\da-zA-Z]+(?:#[\da-zA-Z]+)?|\d+(?:_\d+)*(?:\.\d+(?:_\d+)*)?(?:[eE][+-]?\d+(?:_\d+)*)?)/
+const RE_VARIABLE = /^[A-Z_][a-zA-Z\d_@]*/
+const RE_ATOM = /^[a-z][a-zA-Z\d_@]*/
+const RE_CHARACTER =
+  /^\$(?:\\(?:[0-7]{1,3}|x\{[\da-fA-F]+\}|x[\da-fA-F]{2}|.)|.)/u
+const RE_CONTROL_OPERATOR = /^(?:->|<-)/
+const RE_PUNCTUATION =
+  /^(?:=:=|=\/=|<<|>>|\+\+|--|==|\/=|=<|>=|:=|=>|::|\|\||[()[\]{},.;:+\-*\/=<>!#|&^~])/
+const RE_STRING_CONTENT = /^(?:\\.|[^"\\])+/u
+const RE_QUOTED_ATOM_CONTENT = /^(?:\\.|[^'\\])+/u
+const RE_UNKNOWN = /^./u
 
 export const initialLineState = {
   state: State.TopLevelContent,
   tokens: [],
   stack: [],
 }
+
+export const hasArrayReturn = true
 
 /**
  * @param {any} lineStateA
@@ -69,61 +80,118 @@ export const isEqualLineState = (lineStateA, lineStateB) => {
   return lineStateA.state === lineStateB.state
 }
 
-export const hasArrayReturn = true
+/**
+ * @param {string} line
+ * @param {number} index
+ */
+const isAttributeName = (line, index) => {
+  return line.slice(0, index).trim() === '-'
+}
+
+/**
+ * @param {string} line
+ * @param {number} index
+ */
+const getNextCharacter = (line, index) => {
+  return line.slice(index).match(/^\s*(.)/u)?.[1]
+}
 
 /**
  * @param {string} line
  * @param {any} lineState
  */
 export const tokenizeLine = (line, lineState) => {
-  let next = null
   let index = 0
-  let tokens = []
-  let token = TokenType.None
+  const tokens = []
   let state = lineState.state
-  const stack = lineState.stack
+
   while (index < line.length) {
     const part = line.slice(index)
+    let next
+    let token
+
     switch (state) {
       case State.TopLevelContent:
         if ((next = part.match(RE_WHITESPACE))) {
           token = TokenType.Whitespace
-          state = State.TopLevelContent
-        } else if ((next = part.match(RE_NUMERIC))) {
-          token = TokenType.Numeric
-          state = State.TopLevelContent
-        } else if ((next = part.match(RE_VARIABLE_NAME))) {
-          token = TokenType.Variable
-          state = State.TopLevelContent
-        } else if ((next = part.match(RE_LINE_COMMENT))) {
+        } else if ((next = part.match(RE_COMMENT))) {
           token = TokenType.Comment
-          state = State.TopLevelContent
+        } else if ((next = part.match(RE_CONTROL_KEYWORD))) {
+          token = TokenType.KeywordControl
+        } else if ((next = part.match(RE_KEYWORD))) {
+          token = TokenType.Keyword
+        } else if ((next = part.match(RE_MACRO))) {
+          token = TokenType.Macro
+        } else if ((next = part.match(RE_CHARACTER))) {
+          token = TokenType.String
+        } else if ((next = part.match(RE_NUMBER))) {
+          token = TokenType.Numeric
+        } else if ((next = part.match(RE_VARIABLE))) {
+          token = TokenType.VariableName
+        } else if ((next = part.match(RE_ATOM))) {
+          const nextCharacter = getNextCharacter(line, index + next[0].length)
+          if (isAttributeName(line, index)) {
+            token = TokenType.Keyword
+          } else if (nextCharacter === '(' || nextCharacter === '/') {
+            token = TokenType.Function
+          } else {
+            token = TokenType.LanguageConstant
+          }
+        } else if (part.startsWith('"')) {
+          next = ['"']
+          token = TokenType.PunctuationString
+          state = State.InsideDoubleQuoteString
+        } else if (part.startsWith("'")) {
+          next = ["'"]
+          token = TokenType.PunctuationString
+          state = State.InsideQuotedAtom
+        } else if ((next = part.match(RE_CONTROL_OPERATOR))) {
+          token = TokenType.KeywordControl
         } else if ((next = part.match(RE_PUNCTUATION))) {
           token = TokenType.Punctuation
-          state = State.TopLevelContent
-        } else if ((next = part.match(RE_ANYTHING))) {
-          token = TokenType.Unknown
-          state = State.TopLevelContent
+        } else if ((next = part.match(RE_UNKNOWN))) {
+          token = TokenType.Text
         } else {
-          part //?
-          throw new Error('no')
+          throw new Error('failed to tokenize Erlang source')
         }
         break
+
+      case State.InsideDoubleQuoteString:
+        if (part.startsWith('"')) {
+          next = ['"']
+          token = TokenType.PunctuationString
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_STRING_CONTENT))) {
+          token = TokenType.String
+        } else {
+          throw new Error('failed to tokenize Erlang string')
+        }
+        break
+
+      case State.InsideQuotedAtom:
+        if (part.startsWith("'")) {
+          next = ["'"]
+          token = TokenType.PunctuationString
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_QUOTED_ATOM_CONTENT))) {
+          token = TokenType.LanguageConstant
+        } else {
+          throw new Error('failed to tokenize quoted Erlang atom')
+        }
+        break
+
       default:
-        console.log({ state, line })
-        throw new Error('no')
+        throw new Error(`unknown Erlang tokenizer state: ${state}`)
     }
+
     const tokenLength = next[0].length
     index += tokenLength
     tokens.push(token, tokenLength)
   }
+
   return {
     state,
     tokens,
-    stack,
+    stack: lineState.stack,
   }
 }
-
-// TODO test :hover, :after, :before, ::first-letter
-
-// TODO test complex background image url("data:image/svg+xml,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%206%203'%20enable-background%3D'new%200%200%206%203'%20height%3D'3'%20width%3D'6'%3E%3Cg%20fill%3D'%23b64e4e'%3E%3Cpolygon%20points%3D'5.5%2C0%202.5%2C3%201.1%2C3%204.1%2C0'%2F%3E%3Cpolygon%20points%3D'4%2C0%206%2C2%206%2C0.6%205.4%2C0'%2F%3E%3Cpolygon%20points%3D'0%2C2%201%2C3%202.4%2C3%200%2C0.6'%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")
